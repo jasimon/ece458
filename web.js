aa3a8b62420ac31c0cda1d967907bdc56a558371
Jacob <jacob@madapparelinc.com>
2014-07-26 21:47:56 -0400

    adding post functionality

--- web.js
+++ web.js
@@ -39,6 +39,34 @@ app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
+app.post('/', function(req, res) {
+        var ipAdd = req.headers['x-forwarded-for'];
+    if(ipAdd) {
+        var ipAdds = ipAdd.split(',');
+        ipAdd = ipAdds[ipAdds.length - 1];
+    } else {
+        ipAdd = req.connection.remoteAddress;
+    }
+    console.log(ipAdd);
+        var ua = req.headers['user-agent'];
+    if(/mobile/i.test(ua)) {
+        console.log('mobile detected');
+        console.log(req.body);
+        req.on('data', function (data) {
+            console.log(data.toString());
+        });
+    } else {
+        console.log('desktop detected');
+        var my_g;
+        //while(!my_g) {
+        //    client.get("g", function(err, g) {
+        //        my_g = g;
+        //    });
+        //}
+    }
+    res.send('Hello World!');
+});
+
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
     console.log("Listening on " + port);
