f2e93e855565dc350428ca8df3159f32873dfc36
Jacob <jacob@madapparelinc.com>
2014-07-26 21:08:34 -0400

    adding socket.io and receiving number from phone

--- web.js
+++ web.js
@@ -1,20 +1,42 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var sio = require("socket.io");
+var redis = require("redis");
+
+var client = redis.createClient();
 var app = express();
+var io = sio.listen(app);
+
+io.set("store", new sio.RedisStore);
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
-  var ipAdd = req.headers['x-forwarded-for'];
-  if(ipAdd) {
-    var ipAdds = ipAdd.split(',');
-    ipAdd = ipAdds[ipAdds.length - 1];
-} else {
-    ipAdd = req.connection.remoteAddress;
-}
-  console.log(ipAdd);
-  res.send('Hello World!');
+    var ipAdd = req.headers['x-forwarded-for'];
+    if(ipAdd) {
+        var ipAdds = ipAdd.split(',');
+        ipAdd = ipAdds[ipAdds.length - 1];
+    } else {
+        ipAdd = req.connection.remoteAddress;
+    }
+    console.log(ipAdd);
+    var ua = req.headers['user-agent'];
+    if(/mobile/i.test(ua)) {
+        console.log('mobile detected');
+        req.on('data', function (data) {
+            console.log(data.toString());
+        }
+    } else {
+        console.log('desktop detected');
+        var my_g
+        while(!my_g) {
+            client.get("g", function(err, g) {
+                my_g = g;
+            })
+        }
+    }
+    res.send('Hello World!');
 });
 
 var port = Number(process.env.PORT || 5000);
